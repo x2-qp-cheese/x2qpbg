3dc0b5d4e830418291509c1b176cae904d65774d
GEONWOO PARK <101126026+x2-qp-cheese@users.noreply.github.com>
2025-08-29 05:29:57 +0000

    added gitignore

---
+++ gold5/9084(동전).cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int coin[21];
+
+int main(){
+    int T, n, target;
+    cin >> T;
+
+    for(int i = 0; i < T; i++){
+        cin >> n;
+        for(int i = 0; i < n; i++){
+            cin >> coin[i];
+        }
+        cin >> target;
+        
+    }
+}
