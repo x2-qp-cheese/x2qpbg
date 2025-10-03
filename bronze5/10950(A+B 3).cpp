cf55f33b9b9c7f56a50f1de98d9fd301e1474ab3
GEONWOO PARK <x2qpcheese@gmail.com>
2025-10-03 18:41:42 +0900

    10950: A+B 3

---
+++ bronze5/10950(A+B 3).cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t_case;
+    cin >> t_case;
+    while(t_case--){
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+}
