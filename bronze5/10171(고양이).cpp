c863b2534cb6906b8814b8253208300602f724d5
GEONWOO PARK <x2qpcheese@gmail.com>
2025-10-04 15:25:19 +0900

    10171: 고양이

---
+++ bronze5/10171(고양이).cpp
@@ -0,0 +1,9 @@
+ #include <bits/stdc++.h>
+ using namespace std;
+
+ int main(){
+    cout << "\\    /\\" << "\n";
+    cout << " )  ( ')" << "\n";
+    cout << "(  /  )" << "\n";
+    cout << " \\(__)|" << "\n";
+ }
