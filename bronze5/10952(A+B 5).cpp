ef4e2724e2b1579a140ee449f47e5d9ff8a18885
GEONWOO PARK <101126026+x2-qp-cheese@users.noreply.github.com>
2025-10-06 06:49:41 +0000

    10952: A+B 5

---
+++ bronze5/10952(A+B 5).cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a, b;
+    while(1){
+        cin >> a >> b;
+        if(!a && !b) break;
+        cout << a+b<< "\n";
+    }
+}
